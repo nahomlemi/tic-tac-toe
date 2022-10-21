6e47ecbb1c6c4e3b41e1cfaaf06f0b63a0652558
nahom <nahom lemi>
2022-10-21 23:39:59 +0300

    added function to check for winner.

--- app.rb
+++ app.rb
@@ -65,32 +65,93 @@ class Game < Player
     @@round = 0
     def check_for_win()
         res = [1,3,5]
+
+        if @@round == 9
+            puts "Game Over! Draw"
+            exit
+        elsif @@round <9
+            for i in res
+                if @@b1[i] =='x'&& @@b2[i]=='x'&& @@b3[i] == 'x'
+                    puts "Game Over! Player X have WON!"
+                    exit
+                elsif @@b1[i] =='o'&& @@b2[i]=='o'&& @@b3[i] == 'o'
+                    puts "Game Over! Player O have WON!"
+                    exit
+                end
+                puts i
+            end
+
+
+        elsif @@b1[1] == 'o' && @@b1[3]=='o'&& @@b1[5]=='o';
+            puts "Game Over! Player O have WON!"
+            exit
+        elsif @@b2[1] == 'o'&& @@b2[3]=='o'&& @@b2[5]=='o';
+            puts "Game Over! Player O have WON!"
+            exit
+        elsif @@b3[1] == 'o'&& @@b3[3]=='o'&& @@b3[5]=='o';
+            puts "Game Over! Player O have WON!"
+            exit
+
+        elsif @@b1[1] == 'x'&& @@b1[3]=='x'&& @@b1[5]=='x';
+            puts "Game Over! Player X have WON!"
+            exit
+        elsif @@b2[1] == 'x'&& @@b2[3]=='x'&& @@b2[5]=='x';
+            puts "Game Over! Player X have WON!"
+            exit
+        elsif @@b3[1] == 'x'&& @@b3[3]=='x'&& @@b3[5]=='x';
+            puts "Game Over! Player X have WON!"
+            exit
+
+
+        elsif @@b1[1]=='x'&& @@b2[3]=='x'&& @@b3[5]=='x';
+            puts "Game Over! Player X have WON!"
+            exit
+        elsif @@b1[5]=='x'&& @@b2[3]=='x'&& @@b3[1]=='x';
+            puts "Game Over! Player X have WON!"
+            exit
+        else 
+            puts "hello"
+        end
+    end
+
+=begin
+    def check_for_win()
+        res = [1,3,5]
         if round == '9'
             print  "Game Over! DRAW!"
         elsif for i in res
-            if @@b1[i] =='x';@@b2[i]=='x',@@b3[i] == 'x'
+            if @@b1[i] =='x'&& @@b2[i]=='x'&& @@b3[i] == 'x'
                 puts "Game Over! Player X have WON!"
-            elsif @@b1[i] =='o';@@b2[i]=='o',@@b3[i] == 'o'
+            elsif @@b1[i] =='o'&& @@b2[i]=='o'&& @@b3[i] == 'o'
                 puts "Game Over! Player O have WON!"
 
-            elsif @@b1[1] == 'o';@@b1[3]=='o';@@b1[5]=='o';
+            elsif @@b1[1] == 'o' && @@b1[3]=='o'&& @@b1[5]=='o';
                 puts "Game Over! Player O have WON!"
-            elsif @@b2[1] == 'o';@@b2[3]=='o';@@b2[5]=='o';
+            elsif @@b2[1] == 'o'&& @@b2[3]=='o'&& @@b2[5]=='o';
                 puts "Game Over! Player O have WON!"
-            elsif @@b3[1] == 'o';@@b3[3]=='o';@@b3[5]=='o';
+            elsif @@b3[1] == 'o'&& @@b3[3]=='o'&& @@b3[5]=='o';
                 puts "Game Over! Player O have WON!"
 
-            elsif @@b1[1] == 'x';@@b1[3]=='x';@@b1[5]=='x';
+            elsif @@b1[1] == 'x'&& @@b1[3]=='x'&& @@b1[5]=='x';
                 puts "Game Over! Player X have WON!"
-            elsif @@b2[1] == 'x';@@b2[3]=='x';@@b2[5]=='x';
+            elsif @@b2[1] == 'x'&& @@b2[3]=='x'&& @@b2[5]=='x';
                 puts "Game Over! Player X have WON!"
-            elsif @@b3[1] == 'x';@@b3[3]=='x';@@b3[5]=='x';
+            elsif @@b3[1] == 'x'&& @@b3[3]=='x'&& @@b3[5]=='x';
                 puts "Game Over! Player X have WON!"
 
+            elsif @@b1[1]=='x'&& @@b2[3]=='x'&& @@b3[5]=='x';
+                puts "Game Over! Player X have WON!"
+            elsif @@b1[5]=='x'&& @@b2[3]=='x'&& @@b3[1]=='x';
+                puts "Game Over! Player X have WON!"
 
+            elsif @@b1[1]=='o'&& @@b2[3]=='o'&& @@b3[5]=='o';
+                puts "Game Over! Player O have WON!"
+            elsif @@b1[5]=='o'&& @@b2[3]=='o' && @@b3[1]=='o';
+                puts "Game Over! Player O have WON!"
             end
         end
     end
+=end
     def start_game()
         board()
         while @@winner == false
@@ -102,16 +163,11 @@ class Game < Player
             else 
                 @@current_player = 'x'
             end
+            check_for_win()
+            @@round = @@round + 1
         end
     end
 end
 
 start_game = Game.new()
 start_game.start_game()
-
-
-#board isplay and change score
-#player ask for input and ask board class to display input
-# assign current player,
-#control the flow of the game
-# record score and announce the result
